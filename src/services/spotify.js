237db213a788e245af9c1cf70e01c15844ab49e8
Matheus Kruger <ms-kruger@hotmail.com>
2022-03-21 16:01:50 -0300

    adding volume control

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -66,6 +66,18 @@ async function skipCurrent(token, device_id, method) {
     return result.status === 204 ? await result.text() : await result.json()
 }
 
+async function setVolume(token, device_id, value) {
+    const result = await fetch(`https://api.spotify.com/v1/me/player/volume?volume_percent=${value}&device_id=${device_id}`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + token.access_token,
+            'Content-Type': 'application/json'
+        },
+    })
+
+    return result.status === 204 ? await result.text() : await result.json()
+}
+
 async function getCurrentStatus(token) {
     const result = await fetch(`https://api.spotify.com/v1/me/player`, {
         method: 'GET',
@@ -84,5 +96,6 @@ export {
     listActiveDevices,
     pausePlayCurrent,
     getCurrentStatus,
-    skipCurrent
+    skipCurrent,
+    setVolume
 }
